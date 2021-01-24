2d56c472e04e9d97bbe519e24ecf7381762e41f6
Rolan Zapara <zapara.rolan@gmail.com>
2021-01-24 18:00:06 +0200

    TFP-8 added database for app in docker

--- src/main/java/com/example/foodProject/MainPageController.java
+++ src/main/java/com/example/foodProject/MainPageController.java
@@ -1,12 +1,16 @@
 package com.example.foodProject;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class MainPageController {
     @GetMapping() //baseUrl => localhost:8080
     public String mainPage(
--- src/main/java/com/example/foodProject/repos/MessageRepo.java
+++ src/main/java/com/example/foodProject/repos/MessageRepo.java
@@ -1,7 +1,7 @@
-//package com.example.foodProject.repos;
-//
-//import com.example.foodProject.domain.Message;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface MessageRepo extends CrudRepository<Message, Long> {
-//}
+package com.example.foodProject.repos;
+
+import com.example.foodProject.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+}
